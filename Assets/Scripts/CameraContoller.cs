8cbdfee0b94a1f22ea280aa08015189e7550ec12
FlejeCute <153863899+abigailCode@users.noreply.github.com>
2024-01-17 19:33:48 +0100

    camera OnBecameInvisible

--- Assets/Scripts/CameraContoller.cs
+++ Assets/Scripts/CameraContoller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    [SerializeField] GameObject camera;
     [SerializeField] float yMin;
     [SerializeField] float yMax;
     // Start is called before the first frame update
@@ -13,11 +13,28 @@ public class CameraController : MonoBehaviour
         //AudioManager.instance.PlayMusic("MainTheme");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Mathf.Clamp(1, yMin , yMax);
+    //void Update()
+    //{
+    //    Mathf.Clamp(1, yMin , yMax);
 
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(player.transform.position.y, yMin, yMax), transform.position.z);
+    //    transform.position = new Vector3(transform.position.x, Mathf.Clamp(player.transform.position.y, yMin, yMax), transform.position.z);
+    //}
+
+    //void Update()
+    //{
+    //    float clampedY = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+
+    //    if (player.transform.position.y < yMin || player.transform.position.y > yMax)
+    //    {
+    //        transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+    //    }
+    //}
+
+    void OnBecameInvisible()
+    {
+        if (camera != null)
+        {
+            camera.transform.position = new Vector3(camera.transform.position.x, transform.position.y, camera.transform.position.z);
+        }
     }
 }
