829cf83f5f099ed0a3b5746fa4a688fcb29a320e
abigailCode <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-01-28 20:24:19 +0000

    pause menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale > 0 && Input.GetKeyDown(KeyCode.Escape)){
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }   
+    }
+
+    public void Restart()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+        
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneController.instance.LoadScene("Menu");
+
+    }
+}
